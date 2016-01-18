f8c69b6bbf380992d9a72dc94355079c84cc53e8
Jack Mo <jmo@pivotal.io>
2016-01-18 13:47:08 -0500

    Task 0

---
+++ app/src/androidTest/java/com/example/weatherapp/HelloWorldEspressoTest.java
@@ -0,0 +1,33 @@
+package com.example.weatherapp;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+
+import com.example.weatherapp.activities.MainActivity;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+
+/**
+ * Created by pivotal on 2016-01-18.
+ */
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class HelloWorldEspressoTest {
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule(MainActivity.class);
+
+    @Test
+    public void listGoesOverTheFold() {
+        onView(withText("Today")).check(matches(isDisplayed()));
+    }
+}
+

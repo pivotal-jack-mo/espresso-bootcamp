712c3d4030f4c02ef76fca51d66dc068189699e0
Jack Mo <jmo@pivotal.io>
2016-01-21 16:15:23 -0500

    Task 5 and 6

--- app/src/androidTest/java/com/example/weatherapp/HelloWorldEspressoTest.java
+++ app/src/androidTest/java/com/example/weatherapp/HelloWorldEspressoTest.java
@@ -1,42 +1,40 @@
 package com.example.weatherapp;
 
-import android.app.Application;
-import android.content.Context;
-import android.content.ContextWrapper;
 import android.os.SystemClock;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.contrib.RecyclerViewActions;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
 
 import com.example.weatherapp.activities.MainActivity;
+import com.example.weatherapp.data.WeatherContract;
 
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+
+import java.util.Calendar;
 
 import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
 import static android.support.test.espresso.action.ViewActions.clearText;
 import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.action.ViewActions.replaceText;
 import static android.support.test.espresso.action.ViewActions.swipeDown;
+import static android.support.test.espresso.action.ViewActions.swipeUp;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.CursorMatchers.withRowDouble;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withParent;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
-import static org.hamcrest.Matchers.endsWith;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.hasEntry;
+import static org.hamcrest.Matchers.anyOf;
 import static org.hamcrest.Matchers.hasToString;
-import static org.hamcrest.Matchers.instanceOf;
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.startsWith;
 
 
 /**
@@ -50,38 +48,38 @@ public class HelloWorldEspressoTest{
     public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule(MainActivity.class);
     String location = "Toronto,US";
 
-//    //Task 0
-//    @Test
-//    public void sleep() {
-//        SystemClock.sleep(20000);
-//    }
-//
-//    //Task 1
-//    @Test
-//    public void forecastPreference() {
-//        openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getTargetContext());
-//        onView(withText(R.string.action_settings)).perform(click());
-//        onView(withText(R.string.preference_title)).check(matches(isDisplayed()));
-//    }
-//
-//    //Task 2
-//    @Test
-//    public void changeLocation() {
-//        openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getTargetContext());
-//        onView(withText(R.string.action_settings)).perform(click());
-//        onView(withText(R.string.preference_zip_title)).perform(click());
-//        onView(withId(android.R.id.edit)).perform(clearText()).perform(typeText(location));
-//        onView(withText("OK")).perform(click());
-//        onView(withText(location)).check(matches(isDisplayed()));
-//    }
+    //Task 0
+    @Test
+    public void sleep() {
+        SystemClock.sleep(20000);
+    }
+
+    //Task 1
+    @Test
+    public void forecastPreference() {
+        openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getTargetContext());
+        onView(withText(R.string.action_settings)).perform(click());
+        onView(withText(R.string.preference_title)).check(matches(isDisplayed()));
+    }
+
+    //Task 2
+    @Test
+    public void changeLocation() {
+        openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getTargetContext());
+        onView(withText(R.string.action_settings)).perform(click());
+        onView(withText(R.string.preference_zip_title)).perform(click());
+        onView(withId(android.R.id.edit)).perform(clearText()).perform(typeText(location));
+        onView(withText("OK")).perform(click());
+        onView(withText(location)).check(matches(isDisplayed()));
+    }
 
     //Task 3
-//    @Test
-//    public void refresh() {
-//        onView(withId(R.id.refresh_layout)).perform(swipeDown());
-//        onView(allOf(withId(android.support.design.R.id.snackbar_text), withText(location))).check(matches(isDisplayed()));
-//    }
-//
+    @Test
+    public void refresh() {
+        onView(withId(R.id.refresh_layout)).perform(swipeDown());
+        onView(allOf(withId(android.support.design.R.id.snackbar_text), withText(location))).check(matches(isDisplayed()));
+    }
+
 
     //Task 4
     @Test
@@ -93,5 +91,18 @@ public class HelloWorldEspressoTest{
         onData(hasToString(tempUnit)).perform(click());
     }
 
+    //Task 5
+    @Ignore
+    public void todaysForcast() {
+        Calendar date = Calendar.getInstance();
+        int day = date.get(Calendar.DATE);
+        onData(withRowDouble(WeatherContract.WeatherEntry.COLUMN_DATE, day)).perform(click());
+    }
 
+    //Task 6
+    @Test
+    public void clickLastForecast() {
+        onView(withId(R.id.refresh_layout)).perform(swipeUp());
+        onView(withId(R.id.recyclerview_forecast)).perform(RecyclerViewActions.actionOnItemAtPosition(6, click()));
+    }
 }
